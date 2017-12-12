59417db43157c7fcf7dcae1853f6b7ce72deab27
Antonio Riso <ballacoipruppi@hotmail.com>
2017-12-12 22:00:17 +0100

    init

---
+++ init.pp
@@ -0,0 +1,19 @@
+class jdkariso001a {
+
+  if $::osfamily == 'RedHat' {
+    $jdkname     = 'java-1.8.0-openjdk'
+  } else  {
+    $jdkname  = 'openjdk-8-jdk'
+  }
+
+ package { $jdkname:
+  ensure  => latest,
+
+}
+service { $jdkname:
+    ensure   => 'running',
+    enable  => true,
+    require   => Package[$jdkname],
+}
+
+}
